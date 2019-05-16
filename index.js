2ff549478db189712e5359c2cda57f554380e448
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-05-15 20:32:52 -0700

    Promises working with callbacks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fileReader = require('./lib/reader.js');
+const fileReaderPromise = require('./lib/reader-promises.js');
 
 // Obtain and assert input
 let files = process.argv.slice(2);
@@ -15,3 +16,11 @@ fileReader(files, (err, data) => {
   }
   console.log('From Callback: ', data);
 });
+
+// fileReaderPromise(files)
+//   .then((data) => {
+//     console.log('From Promises: ', data);
+//   })
+//   .catch((err) => {
+//     throw err;
+//   });
